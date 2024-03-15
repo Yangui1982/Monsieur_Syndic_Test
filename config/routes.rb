074dd00adbb9d7a01cddc4ebc38a4b96e2864a7f
JeanGuillaume Vicente <jg.vicente@wanadoo.fr>
2024-03-15 11:17:02 +0100

    navbar desktop done

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "comments#index"
   resources :comments, except: [:edit, :update]
+  get "prix", to: "pages#prices"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
